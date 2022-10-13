b7d230a5f4b43b3fbc55c735be9723242789d18e
Samarth MN <samarth.m.n@gmail.com>
2022-10-13 17:04:02 +0530

    fix: minor style fixes

--- example-app/portal/src/main.tsx
+++ example-app/portal/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'primeicons/primeicons.css'
+import 'primereact/resources/primereact.min.css'
 import '@tesseractcollective/admin-components/dist/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
