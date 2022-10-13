7583d0d6c8d487eff03dedc0e7490610301df15c
Samarth MN <samarth.m.n@gmail.com>
2022-10-13 16:33:22 +0530

    fix: rollup clean up

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,8 @@ import json from '@rollup/plugin-json'
 import { terser } from 'rollup-plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import postcss from 'rollup-plugin-postcss'
-import css from 'rollup-plugin-css-porter'
 import copy from 'rollup-plugin-copy'
 import { createTransform } from 'rollup-copy-transform-css'
-// import sassPlugin from 'rollup-plugin-scss'
-// import postcsss from 'postcss'
-// import autoprefixer from 'autoprefixer'
-// import bundleScss from 'rollup-plugin-bundle-scss'
-// import path from 'path'
 
 import packageJson from './package.json'
 
@@ -34,11 +28,7 @@ export default [
     ],
     plugins: [
       typescript({ tsconfig: './tsconfig.json' }),
-      postcss({
-        // inject(cssVariableName) {
-        //   return `import styleInject from 'style-inject';\nstyleInject(${cssVariableName});`
-        // }
-      }),
+      postcss(),
       commonjs(),
       copy({
         targets: [
@@ -53,17 +43,6 @@ export default [
       json(),
       terser()
     ],
-    external: [...Object.keys(packageJson.dependencies), 'style-inject']
+    external: Object.keys(packageJson.dependencies)
   }
 ]
-// {
-//   failOnError: true,
-//   output: 'dist/styles/index.scss',
-//   exclude: /node_modules/,
-//   includePaths: [
-//     './src/styles/index.css',
-//     './src/styles/theme.css',
-//     './node_modules/primeicons/primeicons.css',
-//     './node_modules/primereact/resources/primereact.min.css'
-//   ]
-// }
