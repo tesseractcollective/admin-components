a512c26f2190e3eb0e62f69b8b0bb9e3ddc7b82b
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2022-10-31 16:27:17 -0600

    Added improved filtering to AdminInputRelation

--- src/Admin/AdminForm/AdminFormInputs/AdminInputRelation.tsx
+++ src/Admin/AdminForm/AdminFormInputs/AdminInputRelation.tsx
@@ -4,19 +4,21 @@ import { classNames } from 'primereact/utils'
 import { Skeleton } from 'primereact/skeleton'
 import { DataAdapter } from '../../DataAdapter'
 import { AdminInputBaseProps, validateProps } from '../AdminForm'
-import { AutoComplete, AutoCompleteCompleteMethodParams, AutoCompleteProps } from 'primereact/autocomplete'
+import { AutoComplete, AutoCompleteCompleteMethodParams } from 'primereact/autocomplete'
+import useDebounce from 'hooks/useDebounce'
 
-interface InputRelationProps {
+export interface AdminInputRelationProps<T extends Record<string, any>> extends AdminInputBaseProps {
   relationshipColumnNameForLabel: string
   relationshipColumnNameForValue: string
   adapter: DataAdapter
-  where?: Record<string, any>
+  labelComparisonOperator?: '_eq' | '_ilike' | '_like'
+  where?: T
+  totalRecordsLimit?: number
 }
 
-export type AdminInputRelationProps = AutoCompleteProps & AdminInputBaseProps & InputRelationProps
 type OptionProps = { value: any; label: string }
 
-export const AdminInputRelation: React.FC<AdminInputRelationProps> = props => {
+export const AdminInputRelation = <T extends Record<string, any>>(props: AdminInputRelationProps<T>) => {
   validateProps(props)
   const {
     control,
@@ -31,6 +33,8 @@ export const AdminInputRelation: React.FC<AdminInputRelationProps> = props => {
     relationshipColumnNameForValue,
     where,
     defaultValue,
+    labelComparisonOperator = '_ilike',
+    totalRecordsLimit = 100,
     onBlur,
     ...baseProps
   } = props
@@ -38,9 +42,12 @@ export const AdminInputRelation: React.FC<AdminInputRelationProps> = props => {
   const priorityLabel = attributeType?.label ?? label ?? name
 
   const [options, setOptions] = useState<OptionProps[]>()
-  const [filteredItems, setFilteredItems] = useState<any>(null)
+  const [filteredItems, setFilteredItems] = useState<OptionProps[]>([])
   const [displayValue, setDisplayValue] = useState<any>(null)
+  const [numberOfItems, setNumberOfItems] = useState<number>(0)
   const [lazyLoading, _setLazyLoading] = useState<any>(false)
+  const [resultsFound, setResultsFound] = useState<boolean>(false)
+  const [defaultResults, setDefaultResults] = useState<OptionProps[] | null>(null)
 
   const itemTemplate = (tOptions: any): ReactNode => {
     const className = classNames('custom-scroll-item scroll-item', {
@@ -60,9 +67,55 @@ export const AdminInputRelation: React.FC<AdminInputRelationProps> = props => {
     )
   }
 
+  const buildwhereClause = (queryString: string): Record<string, any> | undefined => {
+    const userInput = queryString
+    if (userInput && where) {
+      const splitLabelArr = relationshipColumnNameForLabel.split('.') // TODO: make function to DRY
+
+      if (splitLabelArr.length > 1) {
+        const labelWhereClause: Record<string, any> = {}
+
+        // using dot notation will yield AT LEAST 2 items
+        if (splitLabelArr.length > 1) {
+          splitLabelArr.reduce(function (prev, current, currentIndex) {
+            if (currentIndex >= splitLabelArr.length - 1) {
+              return (prev[current] = { [labelComparisonOperator]: `%${userInput}%` })
+            }
+            return (prev[current] = {})
+          }, labelWhereClause)
+        }
+
+        return {
+          _or: [labelWhereClause],
+          _and: [where]
+        }
+      } else {
+        const whereUserInputClauseForLabel: Record<string, any> = {}
+        whereUserInputClauseForLabel[relationshipColumnNameForLabel] = { [labelComparisonOperator]: `%${userInput}%` }
+
+        return {
+          _or: [whereUserInputClauseForLabel],
+          _and: [where]
+        }
+      }
+    } else if (userInput && !where) {
+      // where clause as a prop is optional
+      const whereClause: Record<string, any> = {}
+      whereClause[relationshipColumnNameForLabel] = { _ilike: `%${userInput}%` }
+      return whereClause
+    } else if (!userInput && where) {
+      // userInput empty but a where clause in place
+      return where
+    }
+    return undefined
+  }
+
   const getOptions = useCallback(async (): Promise<void> => {
-    await adapter?.infiniteManyQuery({ where, limit: 100 }).then(({ data }: Record<string, any>) => {
-      const items = data?.current?.map?.((item: Record<string, string>) => {
+    const constructedWhere = buildwhereClause(displayValue as string)
+
+    await adapter?.infiniteManyQuery({ where: constructedWhere, limit: totalRecordsLimit }).then(({ data }: Record<string, any>) => {
+      setNumberOfItems(data?.current?.length || 0)
+      const items: any[] = data?.current?.map?.((item: Record<string, string>) => {
         // we can now use nested object values for label and value e.g "user.bio.firstName"
         const splitValueArr = relationshipColumnNameForValue.split('.')
         const splitLabelArr = relationshipColumnNameForLabel.split('.')
@@ -80,21 +133,74 @@ export const AdminInputRelation: React.FC<AdminInputRelationProps> = props => {
           label: labelData
         }
       })
-      setOptions(items || [])
-    })
-  }, [adapter, relationshipColumnNameForLabel, relationshipColumnNameForValue, where])
 
-  const searchItems = (event: AutoCompleteCompleteMethodParams) => {
-    const userInput = event.query
-    const _filteredItems: any[] = []
-    for (let i = 0; i < options!.length; i++) {
-      const item = options![i]
-      if (item.label.toLowerCase().indexOf(userInput.toLowerCase()) === 0) {
+      if (items.length) {
+        setResultsFound(true)
+        setOptions(items)
+      } else {
+        setResultsFound(false)
+        setOptions([])
+      }
+    })
+  }, [adapter, relationshipColumnNameForLabel, relationshipColumnNameForValue, displayValue, where])
+
+  const debounce = useDebounce(() => {
+    if (!resultsFound) return
+    getOptions()
+  }, 500)
+
+  const searchItemsLocal = (query: string) => {
+    const userInput = query
+    const _filteredItems: OptionProps[] = []
+
+    if (userInput) {
+      for (let i = 0; i < options!.length; i++) {
+        const item = options![i]
+        if (item.label.toLowerCase().indexOf(userInput.toLowerCase()) === 0) {
+          _filteredItems.push(item)
+        }
+      }
+    } else {
+      for (let i = 0; i < options!.length; i++) {
+        const item = options![i]
+        // since we don't have a search query, we insert every item
         _filteredItems.push(item)
       }
     }
-    setFilteredItems(_filteredItems)
+
+    if (_filteredItems.length > 0) {
+      setFilteredItems(_filteredItems)
+    } else {
+      debounce() // as a last resort, it'll try to fetch the server
+    }
   }
+
+  const searchItems = useCallback(
+    (event: AutoCompleteCompleteMethodParams) => {
+      setResultsFound(true) // will make it try to hit the server again
+
+      if (options?.length && filteredItems.length && !event.query) {
+        const defaultResultsCopy = defaultResults?.map(res => res)
+        setFilteredItems(defaultResultsCopy as any[])
+      } else if (numberOfItems < totalRecordsLimit && event.query.length) {
+        searchItemsLocal(event.query)
+      } else {
+        debounce()
+      }
+    },
+    [options, filteredItems, numberOfItems, totalRecordsLimit]
+  )
+
+  useEffect(() => {
+    if (displayValue && resultsFound) {
+      searchItemsLocal(displayValue)
+    } else if (options?.length) {
+      setFilteredItems(options)
+    } else if (!options?.length && !resultsFound) {
+      // Got absolutely nothing
+      setFilteredItems([])
+    }
+  }, [options, displayValue, resultsFound])
 
   useEffect(() => {
     if (!options) {
@@ -103,8 +209,10 @@ export const AdminInputRelation: React.FC<AdminInputRelationProps> = props => {
   }, [getOptions, options])
 
   useEffect(() => {
-    getOptions()
-  }, [getOptions, where])
+    if (options && !defaultResults) {
+      setDefaultResults(options)
+    }
+  }, [options, defaultResults])
 
   return (
     <Controller
